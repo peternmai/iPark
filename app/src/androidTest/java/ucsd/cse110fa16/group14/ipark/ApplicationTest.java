df31a6006544c454a56ce31fe3c41c3dcb3cdd29
Peter Mai <peter2584pm@gmail.com>
2016-10-03 21:21:08 -0700

    Initial commit.

---
+++ app/src/androidTest/java/ucsd/cse110fa16/group14/ipark/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ucsd.cse110fa16.group14.ipark;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/ucsd/cse110fa16/group14/ipark/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ucsd.cse110fa16.group14.ipark;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
