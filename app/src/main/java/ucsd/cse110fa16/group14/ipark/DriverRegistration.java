b1c55dc524ba9f5b9681171830a3a4ef2bc5348c
Maggie <mchemele@ucsd.edu>
2016-10-05 17:31:01 -0700

    created Registration Form for Driver

--- app/src/main/java/ucsd/cse110fa16/group14/ipark/DriverRegistration.java
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/DriverRegistration.java
@@ -1,7 +1,11 @@
 package ucsd.cse110fa16.group14.ipark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DriverRegistration extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class DriverRegistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_registration);
+
+        Button reset = (Button)findViewById(R.id.resetButton);
+        final EditText firstName = (EditText)findViewById(R.id.firstName);
+        final EditText lastName = (EditText)findViewById(R.id.lastName);
+        final EditText email = (EditText)findViewById(R.id.emailAddress);
+        final EditText username = (EditText)findViewById(R.id.userName);
+        final EditText password = (EditText)findViewById(R.id.userPassword);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstName.setText("");
+                lastName.setText("");
+                email.setText("");
+                username.setText("");
+                password.setText("");
+
+            }
+        });
     }
 }
--- app/src/main/java/ucsd/cse110fa16/group14/ipark/LoginScreenActivity.java
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/LoginScreenActivity.java
@@ -1,7 +1,11 @@
 package ucsd.cse110fa16.group14.ipark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginScreenActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class LoginScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        Button regButton = (Button) findViewById(R.id.registerButton);
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreenActivity.this, DriverRegistration.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
