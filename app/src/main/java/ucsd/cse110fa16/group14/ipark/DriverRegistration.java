43d7907c785acad0299fa187a7a02a92abacaf5e
Maggie <mchemele@ucsd.edu>
2016-10-05 17:24:35 -0700

    test

--- app/src/main/java/ucsd/cse110fa16/group14/ipark/DriverRegistration.java
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/DriverRegistration.java
@@ -3,11 +3,11 @@ package ucsd.cse110fa16.group14.ipark;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MakeReservation extends AppCompatActivity {
+public class DriverRegistration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_make_reservation);
+        setContentView(R.layout.activity_driver_registration);
     }
 }
