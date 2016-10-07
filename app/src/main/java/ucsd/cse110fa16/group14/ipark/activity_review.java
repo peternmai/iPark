704be58cd7071521413a07e0cd64981783c53263
Yuyang Chen <yuyangchen@yuyangs-air.dynamic.ucsd.edu>
2016-10-06 19:30:55 -0700

    review

---
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/activity_review.java
@@ -0,0 +1,13 @@
+package ucsd.cse110fa16.group14.ipark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
