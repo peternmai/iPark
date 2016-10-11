5d8c23da1941be72a2d0a545bf0766983992dcba
Maggie <mchemele@ucsd.edu>
2016-10-10 20:30:38 -0700

    created starter navigation bar

---
+++ app/src/main/java/Fragments/ThirdFragment.java
@@ -0,0 +1,26 @@
+package Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ucsd.cse110fa16.group14.ipark.R;
+
+/**
+ * Created by Mag on 10/10/2016.
+ */
+
+public class ThirdFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.activity_clockin,container,false);
+        return myView;
+    }
+}
