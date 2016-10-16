11ca7c45b85c3705c683c319ae36b9d311f29c1b
Maggie <mchemele@ucsd.edu>
2016-10-16 07:28:40 -0700

    Slight changes made to CommentBoss

--- app/src/main/java/ucsd/cse110fa16/group14/ipark/CommentBoss.java
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/CommentBoss.java
@@ -1,10 +1,21 @@
 package ucsd.cse110fa16.group14.ipark;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Mag on 10/13/2016.
  */
 
 public class CommentBoss extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_boss);
+
+    }
 }
