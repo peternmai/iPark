56f82374f3633f5871105a410b383db05a8c1af3
Peter Mai <peter2584pm@gmail.com>
2016-10-17 02:07:43 -0700

    Owner can now view/change parking spot status.  owner can also change individual parking spot's hourly rate.

--- app/src/main/java/ucsd/cse110fa16/group14/ipark/MapInteractive.java
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/MapInteractive.java
@@ -1,10 +1,15 @@
 package ucsd.cse110fa16.group14.ipark;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Point;
+import android.os.Handler;
+import android.text.InputType;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -16,8 +21,15 @@ import java.util.Random;
 
 import android.view.WindowManager;
 import android.view.Display;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+import static android.support.v4.app.ActivityCompat.startActivity;
+import static android.text.InputType.TYPE_NUMBER_FLAG_DECIMAL;
+
 /**
- * Created by jasonallen on 10/8/16.
+ * Created by jasonallen/Peter on 10/8/16.
  */
 
 
@@ -26,6 +38,7 @@ import android.view.Display;
 *  mp = new MapInteractive(this);
 *  setContentView(mp);*/
 
+// Map for owner
 public class MapInteractive extends View {
 
 
@@ -56,8 +69,12 @@ public class MapInteractive extends View {
     //map rectangles to color
     HashMap<Rect, Paint> recCol = new HashMap<Rect, Paint>();
 
+    Context activityContext;
+    Rect activityRect;
+
     private void init(Context context) {
 
+        activityContext = context;
 
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
@@ -149,13 +166,74 @@ public class MapInteractive extends View {
         }
     }
 
+    final Handler handler = new Handler();
+    Runnable longPressed = new Runnable() {
+        public void run() {
+
+            // Reset color change
+            for( int i = 0; i < (paints.length-1); i++)
+                changeRectColor(activityRect);
+
+            System.out.println("Long Press");
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(activityContext);
+
+            builder.setTitle("Advance Setting");
+
+            final EditText input = new EditText(activityContext);
+            input.setInputType(InputType.TYPE_CLASS_NUMBER | TYPE_NUMBER_FLAG_DECIMAL );
+            builder.setView(input);
+
+            builder.setMessage("Current Rate:  $2.50/hr\n" +
+                               "Enter New Parking Rate:");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Todo: handle the OK
+
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+    };
+
     @Override
     public  boolean onTouchEvent(MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
 
-        switch (event.getAction()) {
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                handler.postDelayed(longPressed, 500);
+                for(Rect rect : rectangle) {
+                    if (rect.contains((int) x, (int) y)) {
+                        System.out.println("Touched Rectangle, change color");
+                        changeRectColor(rect);
+                        activityRect = rect;
+                    }
+                }
+                break;
+            /*
+            case MotionEvent.ACTION_MOVE:
+                handler.removeCallbacks(longPressed);
+                break;
+            */
+            case MotionEvent.ACTION_UP:
+                handler.removeCallbacks(longPressed);
+                break;
+        }
 
+        /*
+        switch(event.getAction() ) {
             case MotionEvent.ACTION_DOWN:
 
                 for(Rect rect : rectangle){
@@ -163,8 +241,37 @@ public class MapInteractive extends View {
                     if(rect.contains((int)x,(int)y)){
                         System.out.println("Touched Rectangle, change color");
                         changeRectColor(rect);
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(activityContext);
+
+                        builder.setTitle("Advance Setting");
+
+                        final EditText input = new EditText(activityContext);
+                        input.setInputType(InputType.TYPE_CLASS_NUMBER | TYPE_NUMBER_FLAG_DECIMAL );
+                        builder.setView(input);
+
+                        builder.setMessage("Current Rate:  $2.50/hr\n" +
+                                           "Enter New Parking Rate:");
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Todo: handle the OK
+
+                            }
+                        });
+                        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
                     }
                 }
+
 
                 break;
 
@@ -173,6 +280,8 @@ public class MapInteractive extends View {
                 System.out.println("Touched up");
                 break;
         }
+        */
+
 
         return true;
 
