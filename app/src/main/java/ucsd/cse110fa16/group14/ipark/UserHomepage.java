7833d63b562498b9157488cdaae4035641350011
Maggie <mchemele@ucsd.edu>
2016-10-19 21:39:10 -0700

    Connected activities in User Homepage

--- app/src/main/java/ucsd/cse110fa16/group14/ipark/UserHomepage.java
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/UserHomepage.java
@@ -19,6 +19,10 @@ public class UserHomepage extends AppCompatActivity {
         setContentView(R.layout.activity_user_homepage);
 
         ImageButton logoutButt = (ImageButton) findViewById(R.id.logoutButton);
+        ImageButton reserveButt = (ImageButton) findViewById(R.id.reserveButton);
+        ImageButton checkStatusButt = (ImageButton) findViewById(R.id.checkStatus);
+        ImageButton viewMapButt = (ImageButton) findViewById(R.id.viewMap);
+        ImageButton emergencyButt = (ImageButton) findViewById(R.id.emergency);
 
         logoutButt.setOnClickListener(new View.OnClickListener() {
 
@@ -29,5 +33,40 @@ public class UserHomepage extends AppCompatActivity {
             }
         });
 
+        reserveButt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserHomepage.this, Clockin.class);
+                startActivity(intent);
+            }
+        });
+
+        checkStatusButt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserHomepage.this, CountDownCheckOut.class);
+                startActivity(intent);
+            }
+        });
+
+        viewMapButt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserHomepage.this, MapDirectional.class);
+                startActivity(intent);
+            }
+        });
+
+        emergencyButt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserHomepage.this, BossEmergency.class);
+                startActivity(intent);
+            }
+        });
     }
 }
