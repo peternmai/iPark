dcea60315494d94f9785941fba9b807ef49a5f45
Connie Su <connieli2014@gmail.com>
2016-10-23 12:16:06 -0700

    add confirm report message

--- app/src/main/java/ucsd/cse110fa16/group14/ipark/ReportIllegal.java
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/ReportIllegal.java
@@ -1,13 +1,25 @@
 package ucsd.cse110fa16.group14.ipark;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import static android.R.attr.id;
 
 public class ReportIllegal extends AppCompatActivity {
+    private Context cContext;
+    public void ViewBreakout(Context context) {
+        this.cContext = context;
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -36,6 +48,25 @@ public class ReportIllegal extends AppCompatActivity {
             }
         });
 
+        reportButt.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater inflate = getLayoutInflater();
+                View alertLayout = inflate.inflate(R.layout.confirmreport, null);
+                AlertDialog.Builder confirm = new AlertDialog.Builder(ReportIllegal.this);
+                    confirm.setTitle(R.id.confirmation);
+                    confirm.setPositiveButton(R.id.dialog_ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                         Intent intent = new Intent(cContext, UserReviewHistory.class);
+                        startActivity(intent);
+                    }
+                });
+                AlertDialog alert = confirm.create();
+                alert.show();
+                alert.setContentView(R.layout.confirmreport);
+            }
+        });
     }
 }
