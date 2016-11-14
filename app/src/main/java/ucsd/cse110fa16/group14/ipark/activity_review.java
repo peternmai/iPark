565dbcb383b7a9c928d78846cb8d1035d6562176
Peter Mai <peter2584pm@gmail.com>
2016-11-14 02:04:46 -0800

    Reservation and countdown linked to firebase

--- app/src/main/java/ucsd/cse110fa16/group14/ipark/activity_review.java
+++ app/src/main/java/ucsd/cse110fa16/group14/ipark/activity_review.java
@@ -39,6 +39,7 @@ public class activity_review extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+        iLink.resetUserReservation();
 
         auth = FirebaseAuth.getInstance();
         final String userName = auth.getCurrentUser().getDisplayName();
